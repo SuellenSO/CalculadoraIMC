9f35e8ec9747d855d933977fd29e56461236dd17
Suellen Oliveira <suellenso2020@gmail.com>
2026-05-27 21:01:54 -0400

    css
    mais ajustes e detalhes no css

--- imc-calc/src/components/imcCalc.jsx
+++ imc-calc/src/components/imcCalc.jsx
@@ -1,38 +1,39 @@
 
 import "./imcCalc.css"
+import Button from "./Button"
 
 const ImcCalc = () => {
   return (
-    <div id="calc-container">
-        <h2>Calculadora de IMC</h2>
-        <form id="imc-form">
-            <div className="form-inputs">
-                <div className="form-control">
-                    <label htmlFor="heigth">Altura:</label>
-                    <input 
-                    type="text"  
-                    name="height" 
-                    id="height" 
-                    placeholder="Exemplo 1,75"/>
+        <div id="calc-container">
+            <h2>Calculadora de IMC</h2>
+            <form id="imc-form">
+                <div className="form-inputs">
+                    <div className="form-control">
+                        <label htmlFor="heigth">Altura:</label>
+                        <input 
+                        type="text"  
+                        name="height" 
+                        id="height" 
+                        placeholder="Exemplo 1,75"/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="weigt">Peso:</label>
+                        <input 
+                        type="text"  
+                            name="weight" 
+                        id="weight" 
+                        placeholder="Exemplo 70.5"/>
+                    </div>
                 </div>
-                <div className="form-control">
-                    <label htmlFor="weigt">Peso:</label>
-                    <input 
-                    type="text"  
-                    name="weight" 
-                    id="weight" 
-                    placeholder="Exemplo 70.5"/>
-                </div>
-            </div>
-            <div className="action-control">
-                <Button id="calc-btn" text="Calcular">
-                <Button id="clear-btn" text="Limpar"></Button>
+                <div className="action-control">
+                    <Button id="calc-btn" text="Calcular" />
+                    <Button id="clear-btn" text="Limpar" />
                 
-            </div>
-        </form>
+                </div>
+            </form>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default ImcCalc
