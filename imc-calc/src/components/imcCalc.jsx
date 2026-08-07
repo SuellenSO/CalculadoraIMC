728b136950ba0f4e3e759ed9dadbc1434389c6f6
Suellen Oliveira <suellenso2020@gmail.com>
2026-08-06 22:00:21 -0400

    botão limpar
    criando a função do botão limpar e ajustando o código para que aceite apenas caracteres específicos

--- imc-calc/src/components/imcCalc.jsx
+++ imc-calc/src/components/imcCalc.jsx
@@ -1,8 +1,32 @@
-
+import { useState } from "react"
 import "./imcCalc.css"
 import Button from "./Button"
 
 const ImcCalc = () => {
+  const [height, setHeight] = useState("")
+  const [weight, setWeight] = useState("")
+     
+  const clearForm = (e) => {
+    e.preventDefault();
+    setHeight("");
+    setWeight("");
+  };
+
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "")
+  }
+
+  const handleHeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value)
+    setHeight(updatedValue)
+    }
+    const handleWeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value)
+        setWeight(updatedValue)
+    }
+  
+
+
   return (
         <div id="calc-container">
             <h2>Calculadora de IMC</h2>
@@ -14,20 +38,24 @@ const ImcCalc = () => {
                         type="text"  
                         name="height" 
                         id="height" 
-                        placeholder="Exemplo 1,75"/>
+                        placeholder="Exemplo 1,75"
+                        onChange={(e) => handleHeightChange(e)}
+                        value={height}/>
                     </div>
                     <div className="form-control">
                         <label htmlFor="weigt">Peso:</label>
                         <input 
                         type="text"  
-                            name="weight" 
+                        name="weight" 
                         id="weight" 
-                        placeholder="Exemplo 70.5"/>
+                        placeholder="Exemplo 70.5"
+                        onChange={(e) => handleWeightChange(e)}
+                        value={weight}/>
                     </div>
                 </div>
                 <div className="action-control">
                     <Button id="calc-btn" text="Calcular" />
-                    <Button id="clear-btn" text="Limpar" />
+                    <Button id="clear-btn" text="Limpar" action={clearForm} />
                 
                 </div>
             </form>
